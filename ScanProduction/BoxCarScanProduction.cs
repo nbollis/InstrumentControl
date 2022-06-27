84bf9b35a1610668c3700e3dd7b0b7274c237db7
Nic Bollis <nbollis@comcast.net>
2022-06-27 11:52:11 -0600

    Began Boxcar implementation

---
+++ ScanProduction/BoxCarScanProduction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanProdution
+{
+    public static class BoxCarScanProduction
+    {
+        public static void /*Tuple<double, double>*/ GenerateBoxCars(List<double> mzs)
+        {
+
+        }
+    }
+}
---
+++ ScanProduction/IBoxCarScanOptions.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskInterfaces;
+
+namespace ScanProdution
+{
+    public interface IBoxCarScanOptions : ITaskOptions<IBoxCarScanOptions>
+    {
+        [Option]
+        // with on either side of the selected mass to grab
+        public double DaTolerance { get; set; }
+    }
+
+    public class BoxCarScanOptions : IBoxCarScanOptions
+    {
+        public double DaTolerance { get; set; }
+        public BoxCarScanOptions()
+        {
+
+        }
+
+    }
+}
