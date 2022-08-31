848b8e551f20d8f8722e3709b47289b42635688c
avcarr2 <64652734+avcarr2@users.noreply.github.com>
2022-08-31 16:32:10 -0500

    Removed everything not relevant to the instrument control framework itself

---
+++ ApplicationServer/ExclusionList/ExclusionList.cs
@@ -0,0 +1,68 @@
+﻿using MzLibUtil;
+using ScanProduction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstrumentControl
+{
+    public class ExclusionList 
+    {
+        public List<ExclusionListItem> TheExclusionList { get; set; }
+        public double TimeToExcludeInMilliseconds { get; set; }
+        public PpmTolerance Tolerance { get; set; }
+
+        public ExclusionList()
+        {
+            TheExclusionList = new List<ExclusionListItem>();
+            TimeToExcludeInMilliseconds = ScanProductionGlobalVariables.TimeToExcludeInMilliseconds;
+            Tolerance = new PpmTolerance(ScanProductionGlobalVariables.ExclusionMatchingPpmTolerance);
+        }
+        
+        /// <summary>
+        /// Adds a value to the exclusion list
+        /// </summary>
+        /// <param name="mz"></param>
+        /// <param name="currentTime"></param>
+        public void Add(double mz, double currentTime)
+        {
+            TheExclusionList.Add(new ExclusionListItem(mz, currentTime - TimeToExcludeInMilliseconds, currentTime + TimeToExcludeInMilliseconds));
+        }
+
+        /// <summary>
+        /// Checks to see if the mass is found and meant to be excluded at the current time
+        /// </summary>
+        /// <param name="mass"></param>
+        /// <param name="currentTime"></param>
+        /// <returns>True if the mass is meant to be excluded</returns>
+        public bool ExcludeMassAtCurrentTime(double mass, double currentTime)
+        {
+            var massesWithinTime = TheExclusionList.Where(p => p.WithinTimeSpan(currentTime));
+            if (massesWithinTime.Any(p => Tolerance.Within(mass, p.Mass)))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Exports exclusion list in json format for use in later runs
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void ExportExclusionList()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Imports an exclusion list and adds it to the current list
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
+        public void ImportExclusionList()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ApplicationServer/ThresholdReachedEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thermo.Interfaces.InstrumentAccess_V1.MsScanContainer;
+using Data; 
+
+namespace InstrumentControl
+{
+    public class ThresholdReachedEventArgs : EventArgs
+    {
+        public List<SingleScanDataObject> Data { get; set; }
+        public ThresholdReachedEventArgs(Queue<SingleScanDataObject> dataQueue, int numberElementToDequeue)
+        {
+            Data = new List<SingleScanDataObject>(numberElementToDequeue);
+            DequeueAndSendToList(dataQueue, numberElementToDequeue);
+        }
+        private void DequeueAndSendToList(Queue<SingleScanDataObject> dataQueue, int numberToDequeue)
+        {
+            Data = DequeueMany<SingleScanDataObject>(dataQueue, numberToDequeue).ToList();
+        }
+        private IEnumerable<T> DequeueMany<T>(Queue<T> queue, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                yield return queue.Dequeue();
+            }
+        }
+    }
+}
