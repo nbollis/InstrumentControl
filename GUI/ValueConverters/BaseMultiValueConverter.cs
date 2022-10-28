309543d75965798d32184b3d84fdcfeb797cb3a1
nbollis <nbollis@comcast.net>
2022-10-28 10:16:20 -0500

    Built basic GUI Framework

---
+++ GUI/ValueConverters/BaseMultiValueConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace GUI
+{
+    public abstract class BaseMultiValueConverter : IMultiValueConverter
+    {
+        
+
+        #region Value Converter Methods
+
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+
+        public abstract object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture);
+
+        #endregion
+    }
+}
---
+++ GUI/ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI
+{
+    /// <summary>
+    /// ViewModel for the entire Instrument Control Application
+    /// </summary>
+    public class ApplicationViewModel : BaseViewModel
+    {
+        #region Private Members
+
+
+        #endregion
+
+        #region Public Properties
+
+        public MethodConstructionPageViewModel MethodConstructionPageViewModel { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        #endregion
+
+        #region Constructor
+
+        public ApplicationViewModel()
+        {
+            MethodConstructionPageViewModel = new();
+        }
+
+        #endregion
+
+        #region Private Helpers
+
+        #endregion
+
+    }
+}
